153c6718b53e66f35ef8468d4ac40dc55fce37db
Fede <fedemarc24@gmail.com>
2023-08-05 00:05:30 +0200

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:window_manager/window_manager.dart';
 
+import 'core/left_side.dart';
+import 'core/main_container.dart';
+
 void main() {
   runApp(const MyApp());
   doWhenWindowReady(() async {
@@ -30,8 +33,8 @@ class MyApp extends StatelessWidget {
         body: WindowBorder(
           color: borderColor,
           width: 1,
-          child: const Row(
-            children: [LeftSide(), RightSide()],
+          child:  const Row(
+            children: [RightSide()],
           ),
         ),
       ),
@@ -39,97 +42,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const sidebarColor = Color(0xFFF6A00C);
-
-class LeftSide extends StatelessWidget {
-  const LeftSide({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        width: 200,
-        child: Container(
-            color: sidebarColor,
-            child: Column(
-              children: [
-                WindowTitleBarBox(child: MoveWindow()),
-                Expanded(child: Container())
-              ],
-            )));
-  }
-}
-
-const backgroundStartColor = Color(0xFFFFD500);
-const backgroundEndColor = Color(0xFFF6A00C);
-
-class RightSide extends StatelessWidget {
-  const RightSide({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [backgroundStartColor, backgroundEndColor],
-              stops: [0.0, 1.0]),
-        ),
-        child: Column(children: [
-          WindowTitleBarBox(
-            child: Row(
-              //TODO Implementare TopNavBar inserendo come argomento child: Row() in MoveWindow()
-              children: [Expanded(child: MoveWindow()), const WindowButtons()],
-            ),
-          )
-        ]),
-      ),
-    );
-  }
-}
-
-final buttonColors = WindowButtonColors(
-    iconNormal: const Color(0xFF805306),
-    mouseOver: const Color(0xFFF6A00C),
-    mouseDown: const Color(0xFF805306),
-    iconMouseOver: const Color(0xFF805306),
-    iconMouseDown: const Color(0xFFFFD500));
-
-final closeButtonColors = WindowButtonColors(
-    mouseOver: const Color(0xFFD32F2F),
-    mouseDown: const Color(0xFFB71C1C),
-    iconNormal: const Color(0xFF805306),
-    iconMouseOver: Colors.white);
-
-class WindowButtons extends StatefulWidget {
-  const WindowButtons({Key? key}) : super(key: key);
-
-  @override
-  WindowButtonsState createState() => WindowButtonsState();
-}
-
-class WindowButtonsState extends State<WindowButtons> {
-  void maximizeOrRestore() {
-    setState(() {
-      appWindow.maximizeOrRestore();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        MinimizeWindowButton(colors: buttonColors),
-        appWindow.isMaximized
-            ? RestoreWindowButton(
-          colors: buttonColors,
-          onPressed: maximizeOrRestore,
-        )
-            : MaximizeWindowButton(
-          colors: buttonColors,
-          onPressed: maximizeOrRestore,
-        ),
-        CloseWindowButton(colors: closeButtonColors),
-      ],
-    );
-  }
-}
