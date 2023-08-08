b69e03e8da5a84b8201396fa8c756cf84f8506ca
Fede <fedemarc24@gmail.com>
2023-08-08 21:35:32 +0200

    REFACTOR: moved the top_bar code in top_bar.dart

--- lib/core/main_container.dart
+++ lib/core/main_container.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
-
+import 'package:junction/core/top_bar.dart';
 
 const backgroundStartColor = Colors.black87;
 
@@ -15,78 +15,10 @@ class RightSide extends StatelessWidget {
         ),
         child: Column(children: [
           WindowTitleBarBox(
-            child: Row(
-              //TODO Implementare TopNavBar inserendo come argomento child: Row() in MoveWindow()
-              children: [
-                Expanded(
-                    child: MoveWindow(
-                        child: const Row(
-                  children: <Widget>[
-                    Flexible(
-                      flex: 1,
-                      fit: FlexFit.tight,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 100.0,top: 10,right: 10.0),
-                        child: SearchBar(
-                          hintText: 'Search',
-                        ),
-                      ),
-                    ),
-                  ],
-                ))),
-                const WindowButtons()
-              ],
-            ),
+            child: const TopBar(),
           )
         ]),
       ),
-    );
-  }
-}
-
-final buttonColors = WindowButtonColors(
-    iconNormal: const Color(0xFF805306),
-    mouseOver: const Color(0xFF805306),
-    mouseDown: const Color(0xFF805306),
-    iconMouseOver: backgroundStartColor,
-    iconMouseDown: backgroundStartColor);
-
-final closeButtonColors = WindowButtonColors(
-    mouseOver: const Color(0xFFD32F2F),
-    mouseDown: const Color(0xFFB71C1C),
-    iconNormal: const Color(0xFF805306),
-    iconMouseOver: Colors.white);
-
-class WindowButtons extends StatefulWidget {
-  const WindowButtons({Key? key}) : super(key: key);
-
-  @override
-  WindowButtonsState createState() => WindowButtonsState();
-}
-
-class WindowButtonsState extends State<WindowButtons> {
-  void maximizeOrRestore() {
-    setState(() {
-      appWindow.maximizeOrRestore();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        MinimizeWindowButton(colors: buttonColors),
-        appWindow.isMaximized
-            ? RestoreWindowButton(
-                colors: buttonColors,
-                onPressed: maximizeOrRestore,
-              )
-            : MaximizeWindowButton(
-                colors: buttonColors,
-                onPressed: maximizeOrRestore,
-              ),
-        CloseWindowButton(colors: closeButtonColors),
-      ],
     );
   }
 }
---
+++ lib/core/top_bar.dart
@@ -0,0 +1,91 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/material.dart';
+
+const backgroundStartColor = Colors.black87;
+
+class TopBar extends StatelessWidget {
+  const TopBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Flexible(
+          flex: 1,
+          fit: FlexFit.tight,
+          child: Padding(
+            padding: EdgeInsets.only(left: 100.0, top: 10, right: 10.0),
+            child: SearchBar(
+              hintText: 'Search',
+            ),
+          ),
+        ),
+        WindowButtons(),
+      ],
+    );
+  }
+}
+
+final buttonColors = WindowButtonColors(
+    iconNormal: const Color(0xFF805306),
+    mouseOver: const Color(0xFF805306),
+    mouseDown: const Color(0xFF805306),
+    iconMouseOver: backgroundStartColor,
+    iconMouseDown: backgroundStartColor);
+
+final closeButtonColors = WindowButtonColors(
+    mouseOver: const Color(0xFFD32F2F),
+    mouseDown: const Color(0xFFB71C1C),
+    iconNormal: const Color(0xFF805306),
+    iconMouseOver: Colors.white);
+
+class WindowButtons extends StatefulWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+
+  @override
+  WindowButtonsState createState() => WindowButtonsState();
+}
+
+class WindowButtonsState extends State<WindowButtons> {
+  void maximizeOrRestore() {
+    setState(() {
+      appWindow.maximizeOrRestore();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MinimizeWindowButton(colors: buttonColors),
+        appWindow.isMaximized
+            ? RestoreWindowButton(
+          colors: buttonColors,
+          onPressed: maximizeOrRestore,
+        )
+            : MaximizeWindowButton(
+          colors: buttonColors,
+          onPressed: maximizeOrRestore,
+        ),
+        CloseWindowButton(colors: closeButtonColors),
+      ],
+    );
+  }
+}
+/**
+ * const Row(
+    children: <Widget>[
+    Flexible(
+    flex: 1,
+    fit: FlexFit.tight,
+    child: Padding(
+    padding: EdgeInsets.only(left: 100.0,top: 10,right: 10.0),
+    child: SearchBar(
+    hintText: 'Search',
+    ),
+    ),
+    ),
+    ],
+    )
+ * 
+ * */
