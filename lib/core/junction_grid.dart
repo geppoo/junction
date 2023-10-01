f12464d9224eaca6c00327e1821cf2fd6c4560b4
Fede <fedemarc24@gmail.com>
2023-10-01 18:02:13 +0200

    Minor changes

--- lib/core/junction_grid.dart
+++ lib/core/junction_grid.dart
@@ -36,17 +36,18 @@
 
 import 'package:flutter/material.dart';
 import 'package:junction/core/widget/junction_widget.dart';
-import 'package:junction/core/widget/junction_widget_size.dart';
+import 'package:junction/core/widget/stateful_junction_widget.dart';
 import 'package:junction/core/widget/stateless_junction_widget.dart';
 
 ///
 /// Widget that contains the grid of the junction, it is a stack of [StatelessJunctionWidget] and [StatefulJunctionWidget]
 ///
 class JunctionGrid extends StatefulWidget {
-  List<Widget> junctions = [];
+  final List<Widget> junctions;
   static int padding = 10;
 
-  JunctionGrid({Key? key}) : super(key: key);
+  //TODO check sulle collisioni durante la costruzione della griglia
+  const JunctionGrid({Key? key, required this.junctions}) : super(key: key);
 
 
   /// Add a [StatelessJunctionWidget] or [StatefulWidget] to the grid, returns [true] if the widget is added correctly
@@ -74,7 +75,7 @@ class _JunctionGridState extends State<JunctionGrid> {
   Widget build(BuildContext context) {
     return Stack(
       fit: StackFit.expand,
-      children: widget.junctions ,
+      children: widget.junctions
     );
   }
 }
