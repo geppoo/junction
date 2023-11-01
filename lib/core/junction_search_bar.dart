dab1f9fd656d91556367b13d6f08ce3b956181a9
Fede <fedemarc24@gmail.com>
2023-11-01 10:28:59 +0100

    probable rollback

--- lib/core/junction_search_bar.dart
+++ lib/core/junction_search_bar.dart
@@ -1,45 +1,50 @@
-import 'dart:ffi';
-import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 class JunctionSearchBar extends StatefulWidget {
-  const JunctionSearchBar({super.key});
+  final int numberOfResult;
 
-  @override
-  State<StatefulWidget> createState() => _JunctionSearchBarState(suggestedLength: 0);
+  JunctionSearchBar({Key? key, required this.numberOfResult}) : super(key: key);
+
+      @override
+      State<StatefulWidget> createState() => _JunctionSearchBarState(numberOfResult);
 }
 
 class _JunctionSearchBarState extends State<JunctionSearchBar> {
   late List<String> lastSearch;
   late int suggestedLength;
+
   _JunctionSearchBarState({required this.suggestedLength}) {
     lastSearch = <String>[];
   }
 
   @override
   Widget build(BuildContext context) {
-    return SearchAnchor(builder: (BuildContext context, SearchController controller) {
-      return SearchBar(
-        controller: controller,
-        onTap: () {
-          controller.openView();
-        },
-        onChanged: (_) {
-          controller.openView();
-        },
-        leading: const Icon(Icons.search),
-      );
-    }, suggestionsBuilder: (BuildContext context, SearchController controller) {
-      return List<ListTile>.generate(suggestedLength, (int index) {
-        final String item = lastSearch.length > suggestedLength ? lastSearch[index] : "";
-        return ListTile(
+    return SearchAnchor(
+      builder: (BuildContext context, SearchController controller) {
+        return SearchBar(
+          controller: controller,
+          onTap: () {
+            controller.openView();
+          },
+          onChanged: (_) {
+            controller.openView();
+          },
+          leading: const Icon(Icons.search),
+        );
+      },
+      suggestionsBuilder: (BuildContext context, SearchController controller) {
+        return List<ListTile>.generate(suggestedLength, (int index) {
+          final String item = lastSearch.length > suggestedLength ? lastSearch[index] : "";
+          return ListTile(
             title: Text(item),
             onTap: () {
               setState(() {
                 controller.closeView(item);
               });
-            });
-      });
-    });
+            },
+          );
+        });
+      },
+    );
   }
 }
--- test/searchbar_test.dart
+++ test/searchbar_test.dart
@@ -7,30 +7,24 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:junction/core/junction.dart';
+import 'package:junction/core/junction_search_bar.dart';
 
 import 'package:junction/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('SearchBar return of text', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const JunctionApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    var SUT =  const JunctionSearchBar();
+    await tester.pumpWidget(SUT);
+    await tester.enterText(find.byElementType(SearchBar),"asd");
+    expect( SUT., "asd");
   });
 
 
 
-
 }
+
+
 
 
