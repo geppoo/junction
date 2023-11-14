debf3da2cb6d28802153cc25bf8dff1c9e96b9b9
Fede <federico.marcellifabiani@studenti.unimi.it>
2023-11-14 15:33:55 +0100

    Fixed crash for no history

--- test/search_builder_test.dart
+++ test/search_builder_test.dart
@@ -89,6 +89,18 @@ void main() {
       }
     });
 
+    test('Dynamic length generation', () async {
+      when(doc.ensureInitialized)
+          .thenAnswer((_) async => jsonEncode({'history': []}));
+      when(docS.ensureInitialized).thenAnswer((_) async => jsonEncode({
+            'executable': ['ampl', 'bestie', 'carbon']
+          }));
+      // Act
+      final SearchBuilder SUT = SearchBuilder.testing(doc, docS);
+      final List<Widget> result = await SUT.generateSearch(docSc, 5);
+      expect(result.length, 3);
+    });
+
     testWidgets('tap test', (WidgetTester tester) async {
       when(doc.ensureInitialized).thenAnswer((_) async => jsonEncode({
             'history': ['peppo', 'dwm', 'junction']
