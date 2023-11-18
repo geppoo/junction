7dc6d07fa343f8ae9068a06d82916ef7a775a21e
Geppo <giuseppedeleo160@gmail.com>
2023-11-18 18:55:13 +0100

    Added icon height to junctionWidget bar

--- lib/core/widget/junction_widget_model.dart
+++ lib/core/widget/junction_widget_model.dart
@@ -11,6 +11,7 @@ class JunctionWidgetModel extends StatefulWidget {
   final double width;
   final double bottom;
   final double left;
+  final bool visible;
 
   const JunctionWidgetModel(
       {super.key,
@@ -21,7 +22,8 @@ class JunctionWidgetModel extends StatefulWidget {
       required this.left,
       required this.bottom,
       this.token,
-      this.list});
+      this.list,
+      required this.visible});
 
   @override
   State<JunctionWidgetModel> createState() => _StateJunctionWidget();
@@ -29,53 +31,61 @@ class JunctionWidgetModel extends StatefulWidget {
 
 class _StateJunctionWidget extends State<JunctionWidgetModel> {
   Offset position = const Offset(100, 100);
+  bool visible = true;
 
   void updatePosition(Offset newPosition) =>
       setState(() => position = newPosition);
 
+  void hideWidget(bool value) => setState(() => visible = value);
+
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: position.dx,
-      top: position.dy,
-      child: Column(
-        children: [
-          Container(
-            width: widget.width,
-            height: 20,
-            color: Colors.blueGrey,
-            child: Draggable(
-                maxSimultaneousDrags: 1,
-                feedback: Opacity(
-                  opacity: 0.7,
-                  child: widget,
-                ),
-                childWhenDragging: const Visibility(
-                  visible: false,
-                  child: Text(""),
-                ),
-                onDragEnd: (details) => updatePosition(details.offset),
-                //TODO Implement close bar
-                child: Material(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        onPressed: () => print("Ciao"),
-                        icon: const Icon(Icons.close),
-                      ),
-                    ],
+    return Visibility(
+      visible: visible,
+      child: Positioned(
+        left: position.dx,
+        top: position.dy,
+        child: Column(
+          children: [
+            Container(
+              width: widget.width,
+              height: 20,
+              color: Colors.blueGrey,
+              child: Draggable(
+                  maxSimultaneousDrags: 1,
+                  feedback: Opacity(
+                    opacity: 0.8,
+                    //TODO Change to parent widget for visualization when dragging
+                    child: widget.child,
                   ),
-                )),
-          ),
-          Container(
-            width: widget.width,
-            height: widget.height - 20,
-            color: Colors.grey,
-            //clipBehavior: Clip.hardEdge,
-            child: widget.child,
-          ),
-        ],
+                  childWhenDragging: const Visibility(
+                    visible: false,
+                    child: Text(""),
+                  ),
+                  onDragEnd: (details) => updatePosition(details.offset),
+                  child: Material(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          padding: const EdgeInsets.all(0.0),
+                          onPressed: () => hideWidget(!widget.visible),
+                          icon: const Icon(Icons.close),
+                          iconSize: 15,
+                        ),
+                      ],
+                    ),
+                  )),
+            ),
+            Container(
+              width: widget.width,
+              height: widget.height - 20,
+              color: Colors.grey,
+              //clipBehavior: Clip.hardEdge,
+              child: widget.child,
+            ),
+          ],
+        ),
       ),
     );
   }
