f16ea2ddab6bde9590ef98eeeff42f9d653c572b
Geppo <giuseppedeleo160@gmail.com>
2023-11-25 17:43:26 +0100

    Removed InkWell for hover event on the bar. Added setWindowSize method to junction_model.dart

--- lib/core/junction.dart
+++ lib/core/junction.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hotkey_manager/hotkey_manager.dart';
 import 'package:junction/core/search_bar/junction_search_bar.dart';
 import 'package:junction/dashboard.dart';
+import 'package:junction/main.dart';
 import 'package:provider/provider.dart';
 import '../config/hotkey_bindings.dart';
 import 'expand_button.dart';
@@ -42,58 +43,50 @@ class _JunctionTopBarState extends State<JunctionTopBar> {
         top: 7,
         bottom: 10,
       ),
-      height: expanded ? junctionModel.junctionSettings.junctionBarHeight : 10,
       duration: const Duration(seconds: 1),
       child: Material(
         color: Colors.transparent,
-        child: InkWell(
-          onHover: (isHover) {
-            setState(() {
-              expanded = true;
-            });
-          },
-          child: Column(
-            children: [
-              ///Junction bar
-              SizedBox(
-                width: junctionModel.junctionSettings.junctionBarWidth,
-                height: junctionModel.junctionSettings.junctionBarHeight,
-                child: const Row(
-                  children: [
-                    Expanded(
-                      child: Row(
-                        children: [
-                          Flexible(
-                            flex: 1,
-                            fit: FlexFit.tight,
-                            child: Padding(
-                                padding: EdgeInsets.only(
-                                    left: 1.0, top: 1, bottom: 1, right: 1.0),
-                                child: JunctionSearchBar(
-                                  suggestedLength: 5,
-                                )),
-                          ),
-                        ],
-                      ),
+        child: Column(
+          children: [
+            ///Junction bar
+            SizedBox(
+              width: junctionModel.junctionSettings.junctionBarWidth,
+              height: junctionModel.junctionSettings.junctionBarHeight,
+              child: const Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Flexible(
+                          flex: 1,
+                          fit: FlexFit.tight,
+                          child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: 1.0, top: 1, bottom: 1, right: 1.0),
+                              child: JunctionSearchBar(
+                                suggestedLength: 5,
+                              )),
+                        ),
+                      ],
                     ),
-                    //const WindowButtons(),
-                    ExpandButton(),
-                  ],
+                  ),
+                  //const WindowButtons(),
+                  ExpandButton(),
+                ],
+              ),
+            ),
+
+            ///Expandable Dashboard window
+            Flexible(
+              flex: 1,
+              child: Visibility(
+                visible: junctionModel.isDashboardVisible,
+                child: const Row(
+                  children: [Expanded(flex: 1, child: Dashboard())],
                 ),
               ),
-
-              ///Expandable Dashboard window
-              Flexible(
-                flex: 1,
-                child: Visibility(
-                  visible: junctionModel.isDashboardVisible,
-                  child: const Row(
-                    children: [Expanded(flex: 1, child: Dashboard())],
-                  ),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
